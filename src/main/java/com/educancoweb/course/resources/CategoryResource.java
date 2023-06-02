73db21f70e1690bbbe8853f36e344c56cccdf64f
Vinicius Almeida <vinicius.va2014@gmail.com>
2023-06-02 13:56:21 -0300

    Category

--- src/main/java/com/educancoweb/course/resources/CategoryResource.java
+++ src/main/java/com/educancoweb/course/resources/CategoryResource.java
@@ -9,25 +9,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educancoweb.course.entities.Order;
-import com.educancoweb.course.services.OrderService;
+import com.educancoweb.course.entities.Category;
+import com.educancoweb.course.services.CategoryService;
 
 @RestController
-@RequestMapping(value="/orders")
-public class OrderResource {
+@RequestMapping(value="/categories")
+public class CategoryResource {
 	
 	@Autowired
-	private OrderService service;
-	
+	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll(){
-		List<Order> list = service.findAll();
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
 	@GetMapping(value =  "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id){
-		Order obj = service.findById(id);
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
-}}
+	}
+}
