2428f95fa63002ed2b1dfb3c3f2a6ae153fb5e27
Vinicius Almeida <vinicius.va2014@gmail.com>
2023-06-06 16:25:24 -0300

    Exception handling findByid

--- src/main/java/com/educancoweb/course/services/UserService.java
+++ src/main/java/com/educancoweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educancoweb.course.entities.User;
 import com.educancoweb.course.repositories.UserRepository;
+import com.educancoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -18,10 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repository.findAll();
 		}
-
+	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	public User insert(User obj) {
 		return repository.save(obj);
