339f2a53288a762da3a2d5f241024c57c6c8738e
Vinicius Almeida <vinicius.va2014@gmail.com>
2023-06-06 18:29:04 -0300

    exception handling - update

--- src/main/java/com/educancoweb/course/services/UserService.java
+++ src/main/java/com/educancoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educancoweb.course.repositories.UserRepository;
 import com.educancoweb.course.services.exceptions.DataBaseException;
 import com.educancoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -49,9 +51,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 }
