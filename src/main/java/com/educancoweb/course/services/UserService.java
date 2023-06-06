69a11abfbb07e0c2f0aa5d7dd3ea35d75543da0d
Vinicius Almeida <vinicius.va2014@gmail.com>
2023-06-06 18:13:27 -0300

    Exception handling - delete

--- src/main/java/com/educancoweb/course/services/UserService.java
+++ src/main/java/com/educancoweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educancoweb.course.entities.User;
 import com.educancoweb.course.repositories.UserRepository;
+import com.educancoweb.course.services.exceptions.DataBaseException;
 import com.educancoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -16,30 +19,39 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
-		}
-	
+	}
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
+
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
 	}
-
 
 }
---
+++ src/main/java/com/educancoweb/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.educancoweb.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+}
